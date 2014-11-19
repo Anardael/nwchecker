0df8c17094beecfa4557fede7fd1623ce3e7442b
unknown <digonkin@ch028.softservecom.com>
2014-11-19 18:07:51 +0200

    Added project structure

---
+++ nwcserver/src/com/nwchecker/server/MainController.java
@@ -0,0 +1,18 @@
+package com.nwchecker.server;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/index")
+	public String main(HttpSession session,Model model) throws IllegalArgumentException{
+	
+		return "main";
+	}
+
+}
