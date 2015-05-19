2f4d5686198f3f7e39f19646943732875a6d8bb2
Andy <andycv83@gmail.com>
2015-05-19 16:48:15 +0300

    merge sort and filter

--- nwcserver/src/com/nwchecker/server/dao/ContestDAOImpl.java
+++ nwcserver/src/com/nwchecker/server/dao/ContestDAOImpl.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import javax.transaction.TransactionScoped;
-import javax.transaction.Transactional;
+
 
 @Repository("ContestDAO")
 public class ContestDAOImpl extends HibernateDaoSupport implements ContestDAO {
