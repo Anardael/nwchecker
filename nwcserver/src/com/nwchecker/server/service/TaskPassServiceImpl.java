714858b87484a98bf9c521a9f85a04c9ac39be06
did.yuhim <did.yuhim@gmail.com>
2015-06-14 13:51:35 +0300

    Added test data to existing ongoing competition.

--- nwcserver/src/com/nwchecker/server/service/TaskPassServiceImpl.java
+++ nwcserver/src/com/nwchecker/server/service/TaskPassServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.nwchecker.server.dao.TaskPassDAO;
 import com.nwchecker.server.json.TaskPassJson;
